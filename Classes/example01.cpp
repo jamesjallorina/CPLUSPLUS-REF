976bc7358a348ba17bb0a1226fdfc9a5491088e3
jamesjallorina <jamesmarjun.jallorina@yahoo.com.ph>
2018-09-16 23:04:45 +0800

    added description for example01

--- Classes/example01.cpp
+++ Classes/example01.cpp
@@ -1,10 +1,26 @@
-/************************************
+/**************************************************************************
 *	A simple class implementation
 *	Description	:	use of class as object
 *	Written by	:	James Jallorina
 *	Date		:	09-16-2018
 *	
-*************************************/
+*
+*	Note:
+*	Default constructors
+*	A default constructor is a constructor which can be called with
+*	no arguments (either defined with an empty parameter list, or with 
+*	default arguments provided for every parameter). A type with a public 
+*	default constructor is DefaultConstructible.
+*	
+*	Syntax:
+*	ClassName ( ) ;	(1)	
+*	ClassName :: ClassName ( ) body	(2)	
+*	ClassName() = delete ;	(3)	(since C++11)
+*	ClassName() = default ;	(4)	(since C++11)
+*	ClassName :: ClassName ( ) = default ;	(5)	(since C++11)
+*
+*	source:	https://en.cppreference.com/w/cpp/language/default_constructor
+***************************************************************************/
 
 #include <iostream>
 
