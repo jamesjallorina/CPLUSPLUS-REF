4691c2eea837ee7613164dd39d1569fe1fe97462
jamesjallorina <jamesmarjun.jallorina@yahoo.com.ph>
2018-10-01 19:43:33 +0800

    fixed issues + successfuly compiled and run

--- Classes/default_constructor/example01.cpp
+++ Classes/default_constructor/example01.cpp
@@ -1,20 +1,22 @@
+
 /**************************************************************************
 *	A simple class implementation
 *	Description	:	use of class as object
 *	Written by	:	James Jallorina
 *	Date		:	09-16-2018
-*	
+*
 *
 *	Note:
 *	Default constructors
 *	A default constructor is a constructor which can be called with
-*	no arguments (either defined with an empty parameter list, or with 
-*	default arguments provided for every parameter). A type with a public 
+*	no arguments (either defined with an empty parameter list, or with
+*	default arguments provided for every parameter). A type with a public
 *	default constructor is DefaultConstructible.
-*	
+*
 *	Syntax:
-*	ClassName ( ) ;	(1)	
-*	ClassName :: ClassName ( ) body	(2)	
+
+*	ClassName ( ) ;	(1)
+*	ClassName :: ClassName ( ) body	(2)
 *	ClassName() = delete ;	(3)	(since C++11)
 *	ClassName() = default ;	(4)	(since C++11)
 *	ClassName :: ClassName ( ) = default ;	(5)	(since C++11)
@@ -24,43 +26,45 @@
 
 #include <iostream>
 
+using namespace std;
+
 class human
-{	
-	public:
-		human();
-		~human();
-		//public setter functions
-		void setHeight(int h);
-		void setWeight(int w);
-		//public getter functions
-		int getHeight();
-		int getWeight();
-	
-	private:
-		int height;
-		int weight;
+{
+public:
+	human();
+	~human();
+	//public setter functions
+	void setHeight(double h);
+	void setWeight(double w);
+	//public getter functions
+	double getHeight();
+	double getWeight();
+
+private:
+	double height;
+	double weight;
 };
 
 human::human() {}	//default constructor
-human::~human(){}	//default destructor
+human::~human() {}	//default destructor
 
 
-void human::setWeight(int h)
+void human::setWeight(double h)
 {
 	height = h;
 }
 
-void human::setHeight(int w)
+void human::setHeight(double w)
 {
 	weight = w;
 }
 
-int human::getHeight()
+double human::getHeight()
 {
 	return height;
 }
 
-int human::getWeight()
+double human::getWeight()
 {
 	return weight;
 }
@@ -70,11 +74,11 @@ int human::getWeight()
 int main(int argc, char **argv)
 {
 	human foo;
-	foo.setWeight(50);
+	foo.setWeight(50.0);
 	foo.setHeight(4.5);
-	
-	std::cout << "foo's weight: " << foo.getWeight() << std::ENDL;
-	std::cout << "foo's height: " << foo.getHeight() << std::ENDL;
+
+	cout << "foo's weight: " << foo.getWeight() << endl;
+	cout << "foo's height: " << foo.getHeight() << endl;
 
 	return 0;
 }
