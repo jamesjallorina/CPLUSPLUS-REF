0e91703ae9cbf0588188648858d2bdd96b317101
jamesjallorina <jamesmarjun.jallorina@yahoo.com.ph>
2018-10-01 20:12:21 +0800

    fixed minor issues + successfuly compiled and tested

--- Classes/acess_specifier/example04.cpp
+++ Classes/acess_specifier/example04.cpp
@@ -7,15 +7,15 @@
 *	Written by	:	James Jallorina
 *	Date		:	09-18-2018
 *
-*	Note:	
+*	Note:
 *	access specifiers
-*	In a member-specification of a class/struct or union, define the 
+*	In a member-specification of a class/struct or union, define the
 *	accessibility of subsequent members. In a base-specifier of a derived class
 *	declaration, define the accessibility of inherited members of the subsequent base class.
-*	
-*	
+*
+*
 *	Syntax:
-*	public : member-declarations	(1)	
+*	public : member-declarations	(1)
 *	protected : member-declarations	(2)
 *	private : member-declarations	(3)
 *
@@ -28,23 +28,23 @@
 #include <iostream>
 
 class human
-{	
-	public:
-		human();
-		~human();
-		int public_height;
-		int public_weight;
+{
+public:
+	human();
+	~human();
+	double public_height;
+	double public_weight;
 
-	
-	private:
-		int private_height;
-		int private_weight;
+
+private:
+	double private_height;
+	double private_weight;
 };
 
-human::human() 
+human::human()
 {
-	private_height = 6;
-	private_weight = 75;
+	private_height = 6.0;
+	private_weight = 75.0;
 }	//default constructor
 
 human::~human() {}	//default destructor
@@ -55,14 +55,14 @@ int main(int argc, char **argv)
 {
 	human foo;
 	foo.public_height = 5.7;		//public member are access directly
-	foo.public_weight = 50;
-	
-	std::cout << "foo's public weight: " << foo.public_weight << std::ENDL;
-	std::cout << "foo's public height: " << foo.public_height << std::ENDL;
+	foo.public_weight = 50.0;
 
-	
-	std::cout << "foo's private weight: " << foo.private_weight << std::ENDL;		//this will cause error //we need to provide a public getter functions to 
-	std::cout << "foo's private height: " << foo.private_height << std::ENDL;		// access the private data members
-	
+	std::cout << "foo's public weight: " << foo.public_weight << std::endl;
+	std::cout << "foo's public height: " << foo.public_height << std::endl;
+
+
+	//std::cout << "foo's private weight: " << foo.private_weight << std::endl;		//this will cause error //we need to provide a public getter functions to 
+	//std::cout << "foo's private height: " << foo.private_height << std::endl;		// access the private data members
+
 	return 0;
 }
