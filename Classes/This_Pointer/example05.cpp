02513e91206bc4715c1e71340cca6c0d5027cb98
jamesjallorina <jamesmarjun.jallorina@yahoo.com.ph>
2018-10-01 20:36:26 +0800

    fixed minor issue + succesfully compiled and tested

--- Classes/This_Pointer/example05.cpp
+++ Classes/This_Pointer/example05.cpp
@@ -6,17 +6,17 @@
 *	Written by	:	James Jallorina
 *	Date		:	09-16-2018
 *
-*	Note:	
+*	Note:
 *	this pointer
 *	The keyword this is a prvalue expression whose value is the address of the object,
 *	on which the member function is being called. It can appear in the following contexts:
-*	
+*
 *	1) Within the body of any non-static member function, including member initializer list
-*	2) within the declaration of a non-static member function anywhere after the (optional) 
-*	cv-qualifier sequence, including dynamic exception specification(deprecated), noexcept 
+*	2) within the declaration of a non-static member function anywhere after the (optional)
+*	cv-qualifier sequence, including dynamic exception specification(deprecated), noexcept
 *	specification(C++11), and the trailing return type(since C++11)
 *	3) within default member initializer (since C++11)
-*	
+*
 *
 *
 *	source:	https://en.cppreference.com/w/cpp/language/this
@@ -26,23 +26,24 @@
 
 #include <iostream>
 
+
 class human
-{	
-	public:
-		human();
-		human(int height, int weight);
-		~human();
-		//public setter functions
-		void setHeight(int h);
-		void setWeight(int w);
-		//public getter functions
-		int getHeight();
-		int getWeight();
-		
-	
-	private:
-		int height;
-		int weight;
+{
+public:
+	human();
+	human(double height, double weight);
+	~human();
+	//public setter functions
+	void setHeight(double h);
+	void setWeight(double w);
+	//public getter functions
+	double getHeight();
+	double getWeight();
+
+
+private:
+	double height;
+	double weight;
 };
 
 human::human()
@@ -51,31 +52,31 @@ human::human()
 	weight = 0;
 }
 
-human::human(int height, int weight) 
+human::human(double height, double weight)
 {
 	this->height = height;	//this refer to member variable of an object
 	this->weight = weight;
 }	//default constructor
 
-human::~human(){}	//default destructor
+human::~human() {}	//default destructor
 
 
-void human::setWeight(int height)
-{	
+void human::setWeight(double height)
+{
 	this->height = height;
 }
 
-void human::setHeight(int weight)
+void human::setHeight(double weight)
 {
 	this->weight = weight;
 }
 
-int human::getHeight()
+double human::getHeight()
 {
 	return height;
 }
 
-int human::getWeight()
+double human::getWeight()
 {
 	return weight;
 }
@@ -84,10 +85,11 @@ int human::getWeight()
 
 int main(int argc, char **argv)
 {
-	human foo(5.8, 60);
+	human foo(5.8, 60.0);
+
+	std::cout << "foo's height : " << foo.getHeight() << " (ft)" << std::endl;
+	std::cout << "foo's weight : " << foo.getWeight() << " (kg)" << std::endl;
 	
-	std::cout << "foo's weight : " << foo.getWeight()  << " (kg" << std::ENDL;
-	std::cout << "foo's height :" << foo.getHeight() << " (ft)" << std::ENDL;
 
 	return 0;
 }
