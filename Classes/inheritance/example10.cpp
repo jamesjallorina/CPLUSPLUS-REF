ed45c5b5a1589bc906686018722a5639ce2910de
jamesjallorina <jamesmarjun.jallorina@yahoo.com.ph>
2018-10-01 22:03:25 +0800

    fixed minor issues + successfully compiled and tested

--- Classes/inheritance/example10.cpp
+++ Classes/inheritance/example10.cpp
@@ -5,7 +5,7 @@
 *	Written by	:	James Jallorina
 *	Date		:	09-24-2018
 *
-*	Note:	
+*	Note:
 *	Access				public	protected	private
 *	Same class			yes		yes			yes
 *	Derived classes		yes		yes			no
@@ -18,33 +18,33 @@
 
 #include <iostream>
 
-class human	
-{	
-	public:
-		human();
-		~human();
-		//public setter functions
-		void setHeight(double h);
-		void setWeight(int w);
-		//public getter functions
-		double getHeight();
-		int getWeight();
-	
-	private:
-		double height;
-		int weight;
+class human
+{
+public:
+	human(double h, double w);
+	~human();
+	//public setter functions
+	void setHeight(double h);
+	void setWeight(double w);
+	//public getter functions
+	double getHeight();
+	double getWeight();
+
+private:
+	double height;
+	double weight;
 };
 
-human::human() : height(0), weight(0) {} //default ctor
-human::~human(){}	//default dtor
+human::human(double h = 0.0, double w = 0.0) : height(h), weight(w) {} //default ctor
+human::~human() {}	//default dtor
 
 
-void human::setWeight(int h)
-{	
+void human::setWeight(double h)
+{
 	height = h;
 }
 
-void human::setHeight(int w)
+void human::setHeight(double w)
 {
 	weight = w;
 }
@@ -54,46 +54,44 @@ double human::getHeight()
 	return height;
 }
 
-int human::getWeight()
+double human::getWeight()
 {
 	return weight;
 }
 
 
 class runner : public human
-{	
-	public:
-		runner(){}
-		~runner(){}
-		double getSpeed();
-	private:
-		double distance_run;
-		double time_finished;
-		double speed;
-		double calculate_speed(double distance, double time);
-}
-
-runner::runner(double d = 0.0, double t = 0.0) : distance_run(d), time_finished(t) 
 {
-	speed = 0.0;
-}
+public:
+	runner(double d, double t, double s);
+	~runner();
+	double getSpeed();
+private:
+	double distance_run;
+	double time_finished;
+	double speed;
+	double calculate_speed(double distance, double time);
+};
+
+runner::runner(double d = 0.0, double t = 0.0, double s = 0.0) : distance_run(d), time_finished(t), speed(s) {}
+runner::~runner() {}
 
 double runner::calculate_speed(double distance, double time)
 {
-		speed = distance/time;
-		return speed;
+	speed = distance / time;
+	return speed;
 }
 
 double runner::getSpeed()
 {
-	return calculate_speed(this->distance_run, this->time_finished)
+	return calculate_speed(this->distance_run, this->time_finished);
 }
 
 int main(int argc, char **argv)
 {
-	runner foo(100, 10);
-	
-	std::cout << "speed of runner: " << foo.getSpeed() << std::ENDL;
-	
+	runner foo(100.0, 10.0);
+
+	std::cout << "speed of runner: " << foo.getSpeed() << "m/s" <<std::endl;
+
 	return 0;
 }
