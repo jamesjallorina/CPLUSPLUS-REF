8923af996d2286889e6edaa92fb27c1e6e28d917
jamesjallorina <jamesmarjun.jallorina@yahoo.com.ph>
2018-10-01 21:17:03 +0800

    fixed minor issue + successfully compiled and tested

--- Classes/pointer_to_class_object/example06.cpp
+++ Classes/pointer_to_class_object/example06.cpp
@@ -2,7 +2,7 @@
 *	A simple class implementation
 *	Description	:	use of class as object
 *					1. Pointer to Object
-*	
+*
 *	Written by	:	James Jallorina
 *	Date		:	09-16-2018
 *
@@ -14,22 +14,22 @@
 #include <iostream>
 
 class human
-{	
-	public:
-		human();
-		human(int height, int weight);
-		~human();
-		//public setter functions
-		void setHeight(int h);
-		void setWeight(int w);
-		//public getter functions
-		int getHeight();
-		int getWeight();
-		
-	
-	private:
-		int height;
-		int weight;
+{
+public:
+	human();
+	human(double height, double weight);
+	~human();
+	//public setter functions
+	void setHeight(double h);
+	void setWeight(double w);
+	//public getter functions
+	double getHeight();
+	double getWeight();
+
+
+private:
+	double height;
+	double weight;
 };
 
 human::human()
@@ -38,31 +38,31 @@ human::human()
 	weight = 0;
 }
 
-human::human(int height, int weight) 
+human::human(double height, double weight)
 {
 	this->height = height;	//this refer to member variable of an object
 	this->weight = weight;
 }	//default constructor
 
-human::~human(){}	//default destructor
+human::~human() {}	//default destructor
 
 
-void human::setWeight(int height)
-{	
+void human::setWeight(double height)
+{
 	this->height = height;
 }
 
-void human::setHeight(int weight)
+void human::setHeight(double weight)
 {
 	this->weight = weight;
 }
 
-int human::getHeight()
+double human::getHeight()
 {
 	return height;
 }
 
-int human::getWeight()
+double human::getWeight()
 {
 	return weight;
 }
@@ -71,12 +71,17 @@ int human::getWeight()
 
 int main(int argc, char **argv)
 {
-	human *ptr_foo = new foo(5.7, 58);			//we use pointer to create object that is stored in the heap //later we will use shared_pointer and smart_pointer
-	
-	std::cout << "foo's weight : " << foo->getWeight()  << " (kg" << std::ENDL;
-	std::cout << "foo's height :" << foo->getHeight() << " (ft)" << std::ENDL;
+	human *ptr_foo = new human(5.7, 58.0);			//we use pointer to create object that is stored in the heap //later we will use shared_pointer and smart_pointer
 
-	delete ptr_foo;								//we use delete to release the memory allocated in the heap
-	ptr_foo = NULL;
+	std::cout << "foo's height : " << ptr_foo->getHeight() << " (ft)" << std::endl;
+	std::cout << "foo's weight : " << ptr_foo->getWeight() << " (kg)" << std::endl;
+
+	if (ptr_foo)
+	{
+		std::cout << "deallocate ptr_foo" << std::endl;
+		delete ptr_foo;								//we use delete to release the memory allocated in the heap
+		ptr_foo = NULL;
+	}
+	
 	return 0;
 }
