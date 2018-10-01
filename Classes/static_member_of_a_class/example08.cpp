1218213ba3ac42ac6259c2ecc44167396cb51c1c
jamesjallorina <jamesmarjun.jallorina@yahoo.com.ph>
2018-10-01 21:44:47 +0800

    fixed minor issue + successfuly compiled and tested

--- Classes/static_member_of_a_class/example08.cpp
+++ Classes/static_member_of_a_class/example08.cpp
@@ -5,15 +5,15 @@
 *	Written by	:	James Jallorina
 *	Date		:	09-22-2018
 *
-*	Note:	
+*	Note:
 *	static members
 *	Inside a class definition, the keyword static declares members that are
-*	not bound to class instances. Outside a class definition, it has a different 
+*	not bound to class instances. Outside a class definition, it has a different
 *	meaning: see storage duration.
-*	
+*
 *	Syntax:
-*	static data_member	(1)	
-*	static member_function	(2)		
+*	static data_member	(1)
+*	static member_function	(2)
 *
 *	source:	https://en.cppreference.com/w/cpp/language/static
 ******************************************************************/
@@ -22,49 +22,48 @@
 
 #include <iostream>
 
-class human	
-{	
-	public:
-		human();
-		~human();
-		//public setter functions
-		void setHeight(double h);
-		void setWeight(int w);
-		//public getter functions
-		double getHeight();
-		int getWeight();
-	
-	private:
-		double height;
-		int weight;
-		
-	public:
-		static int total_num_of_human_created;
+class human
+{
+public:
+	human();
+	~human();
+	//public setter functions
+	void setHeight(double h);
+	void setWeight(double w);
+	//public getter functions
+	double getHeight();
+	double getWeight();
+	static int object_created;
+
+private:
+	double height;
+	double weight;
 };
 
-human::human() 
+human::human()
 {
 	height = 0;
 	weight = 0;
-	
-	if(!total_num_of_human_created)
-		std::cout << "one human is created" << std::ENDL;
+
+	if (!object_created)
+		std::cout << "one human is created" << std::endl;
 	else
-		std::cout << "another human is created" << std::ENDL;
-	
-	total_num_of_human_created++;		//increase the human count created everytime this ctor is called
+		std::cout << "another human is created" << std::endl;
+
+	object_created++;		//increase the human count created everytime this ctor is called
 }
+
 human::~human() {}	//default destructor
 
 
-void human::setWeight(int h)
-{	
-	height = h;
+void human::setWeight(double h)
+{
+	weight = h;
 }
 
-void human::setHeight(int w)
+void human::setHeight(double w)
 {
-	weight = w;
+	height = w;
 }
 
 double human::getHeight()
@@ -72,19 +71,19 @@ double human::getHeight()
 	return height;
 }
 
-int human::getWeight()
+double human::getWeight()
 {
 	return weight;
 }
 
-
+int human::object_created = 0;	//This is always required to initialize static member of a class otherwise it is undefined reference
 
 int main(int argc, char **argv)
 {
 	human foo;
 	human scooby;
 
-	std::cout << "total number of humans created" << human::total_num_of_human_created << std::ENDL;
-	
+	std::cout << "total number of humans created" << human::object_created << std::endl;
+
 	return 0;
 }
